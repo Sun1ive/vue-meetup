7369b07d95d232a402a8c9fab51f5b92c5a3d2e2
Sergey <sunliveua@gmail.com>
2017-11-19 14:15:44 +0200

    added routs

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <v-app>
     <v-navigation-drawer v-model="drawer" fixed temporary>
-      <v-list-tile>
+      <v-list-tile v-for="(item, i) in menuItems" :key="i" @click="">
         <v-list-tile-action>
-          <v-icon>supervisor_account</v-icon>
+          <v-icon>{{ item.icon }}</v-icon>
         </v-list-tile-action>
-        <v-list-tile-content>View Meetups</v-list-tile-content>
+        <v-list-tile-content>{{ item.title }}</v-list-tile-content>
       </v-list-tile>
     </v-navigation-drawer>
     <v-toolbar class="primary" dark>
@@ -13,12 +13,12 @@
       <v-toolbar-title>DevMeetup</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-xs-only">
-        <v-btn flat>
-          <v-icon left>supervisor_account</v-icon>View Meetups</v-btn>
+        <v-btn flat v-for="(item, i) in menuItems" :key="i">
+          <v-icon left>{{ item.icon }}</v-icon>{{ item.title }}</v-btn>
       </v-toolbar-items>
     </v-toolbar>
     <main>
-
+      <router-view></router-view>
     </main>
 
   </v-app>
@@ -29,11 +29,20 @@ export default {
   data() {
     return {
       drawer: false,
+      menuItems: [
+        { icon: 'supervisor_account', title: 'View Meetups' },
+        { icon: 'room', title: 'Organize Meetup' },
+        { icon: 'person', title: 'Profile' },
+        { icon: 'face', title: 'Sign up' },
+        { icon: 'lock_open', title: 'Sign in' },
+      ],
     };
   },
 };
 </script>
 
 <style scoped>
-
+li {
+  list-style-type: none;
+}
 </style>
