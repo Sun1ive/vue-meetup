8b3352e1b81f5fcbc475e1627010d3984c6c628d
Sergey <sunliveua@gmail.com>
2017-11-19 16:45:39 +0200

    fix

--- src/components/Meetup/Meetup.vue
+++ src/components/Meetup/Meetup.vue
@@ -25,6 +25,7 @@
 export default {
   props: {
     id: String,
+    required: true,
   },
   computed: {
     meetup() {
--- src/store/index.js
+++ src/store/index.js
@@ -37,10 +37,13 @@ const store = new Vuex.Store({
   actions: {},
   getters: {
     loadedMeetups(state) {
-      return state.loadedMeetups.sort((meetupA, meetupB) => meetupA.date > meetupB.date);
+      return state.loadedMeetups.sort(
+        (meetupA, meetupB) => meetupA.date > meetupB.date,
+      );
     },
     loadedMeetup(state) {
-      return meetupId => state.loadedMeetups.find(meetup => meetup.id === meetupId);
+      return meetupId =>
+        state.loadedMeetups.find(meetup => meetup.id === meetupId);
     },
     featuredMeetups(state, getters) {
       return getters.loadedMeetups.slice(0, 5);
